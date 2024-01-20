d7166dbd8e6a9fa1a2ed13ed20e63d6170fa584c
5Gswatergod <100983649+5Gswatergod@users.noreply.github.com>
2024-01-20 10:09:05 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
-#incldue <iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
-  
+  int n;
+  cin >> n;
+  int score[n];
+  int avg = 0;
+
+  for(int i=0;i<n;i++){
+    cin >> score[i];
+  }
+
+  for(int i=0;i<n;i++){
+    avg+=score[i];
+  }
+
+  if((avg/n)>60){
+    cout << "no";
+  }
+  else{
+    cout << "yes";
+  }
+
   return 0;
 }
